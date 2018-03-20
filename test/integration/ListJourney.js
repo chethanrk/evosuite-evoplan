30597d5adbd621f80d6ec4aaf8060b97a7926a4b
Inamadar Rahul <rahul.inamadar@evorait.com>
2018-03-20 14:02:07 +0000

    Opa Test Continued.

--- test/integration/ListJourney.js
+++ test/integration/ListJourney.js
@@ -4,7 +4,10 @@ sap.ui.define([
 	"use strict";
 
 	QUnit.module("List page");
-
+	 var sDescription = "Test order for EvoPlan integration",
+		 sStatusCode = "INIT",
+		 sDescriptionFalseCase = "11122"
+	 
 	opaTest("Should See the Demand Table", function(Given, When, Then) {
 		Given.iStartTheApp();
 		
@@ -20,13 +23,27 @@ sap.ui.define([
 		and.theTableShouldHaveAllEntries();
 	});
 	
-	/*opaTest("Should Navigate to Demand Overview page", function(Given, When, Then) {
+	opaTest("Should filter entries based on the demand description", function(Given, When, Then) {
 		Given.iStartTheApp();
 
-		When.onTheListPage.iLookAtTheScreen();
+		When.onTheListPage.iSearchWithDemandDecriptionValue(sDescription);
 
-		Then.onTheListPage.iShouldSeeTheTable().
-		and.theTableHasEntries();
-	});*/
+		Then.onTheListPage.iShouldSeeTheTableWithDemandDescription(sDescription).and.iTeardownMyAppFrame();
+	});
+	
+	opaTest("Should filter entries based on the Status", function(Given, When, Then) {
+		Given.iStartTheApp();
+
+		When.onTheListPage.iSearchWithDemandStatusValue(sStatusCode);
+
+		Then.onTheListPage.iShouldSeeTheTableEntriesWithStatus();
+	});
+	opaTest("Should Display the empty table when we search for wrong value", function(Given, When, Then) {
+		Given.iStartTheApp();
+
+		When.onTheListPage.iSearchWithDemandDecriptionValue(sDescriptionFalseCase);
+
+		Then.onTheListPage.iShouldSeeTheEmptyTable();
+	});
 
 });
