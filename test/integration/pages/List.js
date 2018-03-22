c0d1dfe111812b39972ede14655f2eb8583dee6e
Inamadar Rahul <rahul.inamadar@evorait.com>
2018-03-22 08:55:37 +0000

    Opa5 Tests continued.

--- test/integration/pages/List.js
+++ test/integration/pages/List.js
@@ -5,8 +5,9 @@ sap.ui.define([
 	"com/evorait/evoplan/test/integration/pages/Common",
 	"sap/ui/test/matchers/AggregationFilled",
 	"sap/ui/test/matchers/PropertyStrictEquals",
-	"sap/ui/test/matchers/BindingPath"
-], function(Opa5, Press, EnterText, Common, AggregationFilled, PropertyStrictEquals, BindingPath) {
+	"sap/ui/test/matchers/BindingPath",
+	"sap/ui/test/matchers/I18NText"
+], function(Opa5, Press, EnterText, Common, AggregationFilled, PropertyStrictEquals, BindingPath, I18NText) {
 	"use strict";
 	
 	var sViewName = "List",
@@ -20,11 +21,17 @@ sap.ui.define([
 				oModel = oTableBinding.getModel(),
 				aKeys = oTableBinding.aKeys, 
 				bFlag  = false,
-				sStatus = oSearchField.getTokens()[0].getKey();
+				sValue = oSearchField.getTokens()[0].getKey();
 				
 				for(var i in aKeys){
-					var sStatusCode = oModel.getProperty("/"+aKeys[i]+"/UserStatusShortText");
-					if(sStatusCode === sStatus){
+					var sId = oSearchField.getId(),
+						sFilteredValue;
+					if(sId.search("/DemandDesc/i") === -1)
+						sFilteredValue = oModel.getProperty("/"+aKeys[i]+"/UserStatusShortText");
+					else
+						sFilteredValue = oModel.getProperty("/"+aKeys[i]+"/DemandDesc");
+						
+					if(sFilteredValue === sValue){
 						bFlag = true;
 					}else{
 						bFlag = false;
@@ -40,7 +47,7 @@ sap.ui.define([
 				id : sTableId,
 				viewName : sViewName,
 				matchers : function (oTable) {
-					return oTable.getTable().getAggregation("rows")[iPosition].getAggregation("rowActionTemplate").getAggregation("items")[0];
+					return oTable.getTable().getAggregation("rows")[iPosition];
 				},
 				actions : oOptions.actions,
 				success : oOptions.success,
@@ -60,8 +67,28 @@ sap.ui.define([
 				iPressATableItemAtPosition :  function (iPosition) {
 						return this.waitFor(createWaitForItemAtPosition({
 							position : iPosition,
+							success:function(oRow){
+								var oRowAction = oRow.getAggregation("_rowAction").getItems()[0];
+								oRowAction.firePress({
+									row:oRow
+								});
+							},
 							actions : new Press()
 						}));
+				},
+				iPressATableItemAtPosition1 :  function (iPosition) {
+						return this.waitFor({
+							id : sTableId,
+							viewName : sViewName,
+							success:function(oTable){
+								var oRow = oTable.getTable().getAggregation("rows")[0];
+								var oRowAction = oRow.getAggregation("_rowAction").getItems()[0];
+								oRowAction.firePress({
+									row:oRow
+								});
+							},
+							errorMessage : "Can't see the Demand table."
+						});
 				},
 				iSearchWithDemandDecriptionValue :  function (sText) {
 						this.waitFor({
@@ -91,7 +118,40 @@ sap.ui.define([
 							viewName : sViewName,
 							actions: new Press()
 						});
-				}
+				},
+				iSelectDemandFromDemandTable:function(){
+					return this.waitFor({
+							id : sTableId,
+							viewName : sViewName,
+							success : function (oTable) {
+								oTable.getTable().setSelectedIndex(1);
+							},
+							errorMessage : "Can't see the Demand Table."
+						});
+				},
+				iClickOnTheAboutIcon:function(){
+					return this.waitFor({
+							id : "idButtonAboutDialog",
+							viewName : sViewName,
+							actions: new Press(),
+							errorMessage : "Can't see the About Button."
+						});
+				},
+				iRememberTheItemAtPosition : function (iPosition){
+						return this.waitFor(createWaitForItemAtPosition({
+							position : iPosition,
+							success : function (oTableItem) {
+								var oBindingContext = oTableItem.getBindingContext();
+
+								// Don't remember objects just strings since IE will not allow accessing objects of destroyed frames
+								this.getContext().currentItem = {
+									bindingPath: oBindingContext.getPath(),
+									id: oBindingContext.getProperty("ObjectID"),
+									name: oBindingContext.getProperty("Name")
+								};
+							}
+						}));
+					}
 			}),
 			assertions: jQuery.extend({
 				iShouldSeeTheTable : function () {
@@ -141,16 +201,30 @@ sap.ui.define([
 							errorMessage : "was not able see the Filter bar"
 						});
 					},
-				iShouldSeeTheAssignButtonAsDisabled : function () {
+				iShouldSeeTheAssignButtonAs : function (bEnabled) {
 						return this.waitFor({
 							viewName : sViewName,
 							id : "assignButton",
 							matchers : new PropertyStrictEquals({
 								name:"enabled",
-								value:false
+								value:bEnabled
 							}),
 							success : function () {
 								Opa5.assert.ok(true, "Assign button is visible and it is disabled");
+							},
+							errorMessage : "Was not able see the Assign button or Assign button is enabled"
+						});
+					},
+					iShouldSeeTheChangeStatusButtonAs:function(bEnabled){
+						return this.waitFor({
+							viewName : sViewName,
+							id : "changeStatusButton",
+							matchers : new PropertyStrictEquals({
+								name:"enabled",
+								value:bEnabled
+							}),
+							success : function () {
+								Opa5.assert.ok(true, "Change Status button is visible and it is disabled");
 							},
 							errorMessage : "Was not able see the Assign button or Assign button is enabled"
 						});
@@ -160,7 +234,7 @@ sap.ui.define([
 							controlType: "sap.ui.table.Row",
 							viewName : sViewName,
 							matchers: new BindingPath({
-								path: "/DemandSet('0A51491BD5A01ED88AAE865216887184')"
+								path: "/DemandSet('0A51491BD5A01ED88AEE3E1FEF5FF9F3')"
 							}),
 							success : function (aRows) {
 								var oContext = aRows[0].getBindingContext(),
@@ -174,15 +248,15 @@ sap.ui.define([
 							errorMessage : "The is not filtered correctly"
 						});
 					},
-					iShouldSeeTheTableEntriesWithStatus:function(){
+					iShouldSeeTheTableEntriesWithStatus:function(sStatus){
 						return this.waitFor({
 							id : [sTableId, createIdFor(sFilter, "UserStatusShortText")],
 							viewName : sViewName,
 							check:  allItemsInTheListContainTheSearchTerm,
 							success : function () {
-								Opa5.assert.ok(true, "Every item did contain the Status");
+								Opa5.assert.ok(true, "Every item did contain the Status "+sStatus);
 							},
-							errorMessage : "The table did not have items"
+							errorMessage : "The table did not have Demands"
 						});
 					},
 					iShouldSeeTheEmptyTable:function(){
@@ -195,6 +269,31 @@ sap.ui.define([
 							},
 							success: function() {
 								Opa5.assert.ok(true, "Table has no rows!");
+							}
+						});
+					},
+					iShouldSeeTheDemandOverviewPageWithTitle:function(sTitle){
+						return this.waitFor({
+							id:"objectPage",
+							viewName:"Detail",
+							matchers: new I18NText({
+								propertyName:"title",
+                                key: sTitle
+                            }),
+							success: function(sTitleText) {
+								Opa5.assert.ok(true, "Navigated to Demand overview page with title Demand Overview");
+							}
+						});
+					},
+					iShouldSeeTheInformationPopupWithTitle:function(sTitle){
+						return this.waitFor({
+							controlType: "sap.m.Dialog",
+							matchers: new I18NText({
+								propertyName:"title",
+                                key: sTitle
+                            }),
+							success: function(sTitleText) {
+								Opa5.assert.ok(true, "The infomation pop up with title About");
 							}
 						});
 					},
