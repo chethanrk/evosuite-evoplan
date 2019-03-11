77edb1d9798dc78bd3a947a5a0d4ac09e4443179
Michaela Schlage <michaela.schlage@evorait.com>
2019-03-11 16:54:27 +0100

    Commit ResourceTree SMartFilterBar based on new master branch

--- webapp/controller/ResourceTreeFilterBar.js
+++ webapp/controller/ResourceTreeFilterBar.js
@@ -19,11 +19,22 @@ sap.ui.define([
 
 		_oFilterBar: null,
 
+		_aCustomFilters: ["BasicSearch", "NodeType", "ResourceGroupToken"],
+
+		_oOldCustomFilterNames: {
+			BasicSearch: "searchField",
+			NodeType: "viewFilterItem",
+            ResourceGroupToken: "multiGroupInput",
+            StartDate: "dateRange1",
+            EndDate: "dateRange2"
+		},
+
 		init: function (oView, sControlId) {
 			this._oView = oView;
 			var oFragment = sap.ui.xmlfragment("com.evorait.evoplan.view.fragments.ResourceTreeFilterBar", this);
 			var oLayout = oView.byId(sControlId);
 			this._oFilterBar = sap.ui.getCore().byId("resourceTreeFilterBar");
+			this._oVariantMangement = this._oFilterBar.getVariantManagement();
 
 			// connect filterbar to view (models, lifecycle)
 			oLayout.addContent(oFragment);
@@ -48,21 +59,52 @@ sap.ui.define([
 			oEvent.getSource().setProperty("filterSuggests", true);
 		},
 
-		onBeforeVariantSave: function (oEvent) {},
+        /**
+		 *
+         * @param oEvent
+         */
+        onInitialized: function (oEvent) {
+            console.log("onInitialized");
+            var oSource = oEvent.getSource();
 
-		onBeforeVariantFetch: function (oEvent) {},
+        },
 
-		onFilterChange: function (oEvent) {},
+        onFilterChange: function (oEvent) {
+            console.log("onFilterChange");
+        },
 
+		onBeforeVariantSave: function (oEvent) {
+            if (oEvent.getParameter("context") === "STANDARD") {
+                this._updateCustomFilter();
+            }
+		},
+
+		onBeforeVariantFetch: function (oEvent) {
+            this._updateCustomFilter();
+		},
+
+
+        /**
+		 * Todo: set custom fields
+         * @param oEvent
+         */
 		onAfterVariantLoad: function (oEvent) {
-			//Todo: set custom fields
-			if (this._oFilterBar) {
-				var oData = this._oFilterBar.getFilterData();
-				var oCustomFieldData = oData["_CUSTOM"];
-				if (oCustomFieldData) {
-					var oCtrl = this._oFilterBar.determineControlByName("MyOwnFilterField");
-					if (oCtrl) {
-						oCtrl.setSelectedKey(oCustomFieldData.MyOwnFilterField);
+            console.log("onAfterVariantLoad");
+			var oData = this._oFilterBar.getFilterData(),
+				oCustomFieldData = oData["_CUSTOM"];
+
+			if (oCustomFieldData) {
+				for (var i = 0; i < this._aCustomFilters.length; i++) {
+					var sFilterKey = this._aCustomFilters[i],
+						oCtrl = this._oFilterBar.determineControlByName(sFilterKey);
+
+					if(oCtrl){
+						try{
+							oCtrl.setSelectedKey(oCustomFieldData[sFilterKey]);
+						}catch (e){ /*do nothing*/ }
+						try{
+							oCtrl.setValue(oCustomFieldData[sFilterKey]);
+						}catch (e){ /*do nothing*/ }
 					}
 				}
 			}
@@ -70,7 +112,30 @@ sap.ui.define([
 
 		onExit: function () {
 
-		}
+		},
+
+        /**
+		 *
+         * @private
+         */
+        _updateCustomFilter: function () {
+            if (this._oFilterBar) {
+            	var oCustom = { _CUSTOM : {}};
+
+                for (var i = 0; i < this._aCustomFilters.length; i++) {
+                    var sFilterKey = this._aCustomFilters[i],
+                        oCtrl = this._oFilterBar.determineControlByName(sFilterKey);
+                    if (oCtrl) {
+                    	oCustom._CUSTOM[sFilterKey] = oCtrl.getSelectedKey();
+                        this._oFilterBar.setFilterData(oCustom);
+                    }
+                }
+            }
+        },
+		
+		_getCustomFilterQuery: function () {
+			
+        }
 	});
 
 });
