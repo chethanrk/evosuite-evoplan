d3314f3f8e9be13704094861df091fa86de88e9c
Inamadar Rahul <rahul.inamadar@evorait.com>
2019-04-09 11:46:53 +0000

    Custom Title refactoring.

--- webapp/ui/controls/CustomTitleRenderer.js
+++ webapp/ui/controls/CustomTitleRenderer.js
@@ -1,73 +1,23 @@
-sap.ui.define([
-    "sap/m/Title",
-    "sap/ui/core/Icon",
-    "sap/ui/core/IconPool"
-], function (Title, Icon, IconPool) {
+/*!
+ * Custom title for resource tree
+ */
 
-    var CustomTitle = Title.extend("com.evorait.evoplan.ui.controls.CustomTitle", {
-        metadata: {
-            properties: {
-                /**
-                 * Defines the URL for icon display
-				 * isCapacityEnabled {String} Is capacitive mode in enable
-				 * showCapacity {boolean} false value will hide the worktime information
-                 */
-                icon: {
-                    type: "sap.ui.core.URI",
-                    group: "Appearance",
-                    defaultValue: null
-                },
-                isLink: {
-                    type: "boolean",
-                    group: "Misc",
-                    defaultValue: false
-                },
-                availabilityIcon: {
-                    type: "sap.ui.core.URI",
-                    group: "Appearance",
-                    defaultValue: null
-                },
-                iconTooltip: {
-                    type: "string",
-                    group: "Misc",
-                    defaultValue: null
-                },
-                iconColor: {
-                    type: "sap.ui.core.CSSColor",
-                    group: "Appearance",
-                    defaultValue: "orange"
-                },
-				workTime: {
-                    type: "string",
-                    group: "Misc",
-                    defaultValue: 8
-				},
-				isWorkTimeEnabled:{
-                    type: "boolean",
-                    group: "Misc",
-                    defaultValue: false
-				},
-				showWorkTime: {
-                    type: "boolean",
-                    group: "Misc",
-                    defaultValue: false
-				}
-            },
-            events: {
-                /**
-                 * This event is fired when click on title.
-                 */
-                press: {
-                    allowPreventDefault: true
-                }
-            }
-        }
-       /* renderer: function (oRm, oControl) {
+// Provides default renderer for control CustomTitle
+sap.ui.define([
+    "sap/ui/core/Renderer",
+        "sap/ui/core/Icon",
+        "sap/ui/core/IconPool"],
+    function(Renderer,Icon,IconPool) {
+        "use strict";
+
+        var CustomTitleRenderer = {};
+        CustomTitleRenderer.render = function(oRm, oControl) {
+
             var oIconInfo = IconPool.getIconInfo(oControl.getIcon());
             var oAvailabilityIcon = IconPool.getIconInfo(oControl.getAvailabilityIcon());
             var oAssoTitle = oControl._getTitle(),
                 sLevel = (oAssoTitle ? oAssoTitle.getLevel() : oControl.getLevel()) || sap.ui.core.TitleLevel.Auto,
-                bAutoLevel = sLevel == sap.ui.core.TitleLevel.Auto,
+                bAutoLevel = sLevel === sap.ui.core.TitleLevel.Auto,
                 sTag = bAutoLevel ? "div" : sLevel,
                 isLink = oControl.getProperty("isLink"),
                 titleText = oAssoTitle ? oAssoTitle.getText() : oControl.getText();
@@ -87,7 +37,7 @@ sap.ui.define([
             }
 
             var sTextAlign = oControl.getTextAlign();
-            if (sTextAlign && sTextAlign != sap.ui.core.TextAlign.Initial) {
+            if (sTextAlign && sTextAlign !== sap.ui.core.TextAlign.Initial) {
                 oRm.addClass("sapMTitleAlign" + sTextAlign);
             }
 
@@ -127,10 +77,10 @@ sap.ui.define([
 
                 if(!oControl.getIsWorkTimeEnabled()) {
                     // Availability icon
-                    oControl._renderAvailabilityIcon(oRm, oIconInfo, oAvailabilityIcon,oControl);
+                    this.renderAvailabilityIcon(oRm, oIconInfo, oAvailabilityIcon,oControl);
                 }else {
-                	if(oControl.getShowWorkTime()){
-                	    oControl._renderWorkTimeInfo(oRm, oControl);
+                    if(oControl.getShowWorkTime()){
+                        this.renderWorkTimeInfo(oRm, oControl);
                     }
                 }
             }
@@ -160,16 +110,17 @@ sap.ui.define([
             }
 
             oRm.write("</", sTag, ">");
-        },
-        /!**
+        };
+
+        /**
          * Create the HTML Content for Availability Icon
          *
          * @param oRm Render manager
          * @param oAvailabilityIcon Availability icon
          * @param oControl Custom title
          * @private
-         *!/
-        _renderAvailabilityIcon : function (oRm, oIconInfo, oAvailabilityIcon, oControl) {
+         */
+        CustomTitleRenderer.renderAvailabilityIcon = function (oRm, oIconInfo, oAvailabilityIcon, oControl) {
             oRm.write("<span");
             oRm.addClass("sapUiIcon");
             oRm.addClass("sapUiTinyMarginBegin");
@@ -182,18 +133,17 @@ sap.ui.define([
             oRm.writeClasses();
             oRm.writeStyles();
             oRm.write("></span>");
-        },
-        /!**
+        };
+        /**
          * Create the HTML content to show the Work time information
          * @param oRm Render manager
          * @param oControl Custom title
          * @private
-         *!/
-        _renderWorkTimeInfo : function (oRm, oControl) {
+         */
+        CustomTitleRenderer.renderWorkTimeInfo = function (oRm, oControl) {
             oRm.write("<div");
             oRm.addClass("worktimeAvailability");
             oRm.addClass("sapUiTinyMarginBegin");
-            oRm.addStyle("display", "inline-block");
             oRm.writeAttributeEscaped("title", oControl.getIconTooltip());
             oRm.addStyle("background-color", oControl.getIconColor());
             if(oControl.getIconColor() === ""){
@@ -215,31 +165,7 @@ sap.ui.define([
             oRm.writeStyles();
             oRm.write(">"+oControl.getWorkTime()+"</span>");
             oRm.write("</div>");
-        }*/
-    });
+        };
 
-    CustomTitle.prototype.setIcon = function (sUrl) {
-        this.setProperty("icon", sUrl);
-    };
-
-    CustomTitle.prototype.setIsLink = function (bool) {
-        this.setProperty("isLink", bool);
-    };
-
-    CustomTitle.prototype._handlePress = function (oEvent) {
-        if (this.getIsLink()) {
-            oEvent.setMarked();
-            if (!this.firePress()) { // fire event and check return value whether default action should be prevented
-                oEvent.preventDefault();
-            }
-        }
-    };
-
-    if (sap.ui.Device.support.touch) {
-        CustomTitle.prototype.ontap = CustomTitle.prototype._handlePress;
-    } else {
-        CustomTitle.prototype.onclick = CustomTitle.prototype._handlePress;
-    }
-
-    return CustomTitle;
-});
+        return CustomTitleRenderer;
+    },true);
