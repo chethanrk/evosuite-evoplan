8b5d2dabecc8c51a82c95f48f8d816cf686b2aea
Rakesh Sahu <rakesh.sahu@evorait.com>
2020-10-15 03:30:40 +0000

    Started working on Reassignment Qualification check

--- webapp/controller/common/AssignmentsController.js
+++ webapp/controller/common/AssignmentsController.js
@@ -163,7 +163,13 @@ sap.ui.define([
 			if (isReassign && oData.NewAssignPath && !this.isAvailable(oData.NewAssignPath)) {
 				this.showMessageToProceed(null, null, null, null, true, oParams, mParameters);
 			} else {
-				this.callFunctionImport(oParams, "UpdateAssignment", "POST", mParameters, true);
+				if (this.getModel("user").getProperty("/QUALIF_UI_CHECK_TYPE")) {
+					//TODO Check Qualification
+				} else {
+					alert("Update Assignment Function Call");
+					return;
+					this.callFunctionImport(oParams, "UpdateAssignment", "POST", mParameters, true);
+				}
 			}
 		},
 		/**
@@ -176,6 +182,15 @@ sap.ui.define([
 		 * @param aPaths {Array} selected assignment paths
 		 */
 		bulkReAssignment: function (sAssignPath, aContexts, mParameters) {
+			if (this.getModel("user").getProperty("/QUALIF_UI_CHECK_TYPE")) {
+				//TODO Check Qualification
+			} else {
+				this.bulkReAssignmentFinalCall(sAssignPath, aContexts, mParameters);
+			}
+		},
+		bulkReAssignmentFinalCall: function (sAssignPath, aContexts, mParameters) {
+			alert("BulkReassignment Function Call");
+			return;
 			var oModel = this.getModel(),
 				oResource = oModel.getProperty(sAssignPath),
 				bIsLast = null;
