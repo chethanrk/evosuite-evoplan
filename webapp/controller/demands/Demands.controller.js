3736b31303b804a10f30d224846ad6464219b6fb
Chethan RK <chethan.rk@evorait.com>
2021-11-08 08:13:14 +0000

    Merging 2106.0.1 hotfix to refactoring_development

--- webapp/controller/demands/Demands.controller.js
+++ webapp/controller/demands/Demands.controller.js
@@ -202,6 +202,9 @@ sap.ui.define([
 				aIndices = this._oDataTable.getSelectedIndices(),
 				oSelectedPaths, aPathsData;
 
+			//Restricting selected demand list as per the global config select all property 
+			aIndices = aIndices.slice(0, this.getModel("user").getProperty("/DEFAULT_DEMAND_SELECT_ALL"));
+
 			oDragSession.setTextData("Hi I am dragging");
 			//get all selected rows when checkboxes in table selected
 			if (aIndices.length > 0) {
@@ -516,7 +519,7 @@ sap.ui.define([
 				}
 			}
 		},
-		
+
 		/**
 		 * Opens long text view/edit popover
 		 * @param {sap.ui.base.Event} oEvent - press event for the long text button
