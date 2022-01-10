632482670e2fc67a5a23b697316e9157e920abf5
Pranav Kumar <pranav.kumar@evorait.com>
2022-01-10 04:00:47 +0000

    Entity name has changed with new name

--- webapp/Component.js
+++ webapp/Component.js
@@ -593,7 +593,7 @@ sap.ui.define([
 		 * @private
 		 */
 		_getAvailabilityGroup: function (sAvailabilityTypeGroup) {
-			this.getModel().read("/AvailabilityTypeSet", {
+			this.getModel().read("/SHAvailabilityTypeSet", {
 				filters: [
 					new Filter("AvailabilityTypeGroup", FilterOperator.EQ, sAvailabilityTypeGroup)
 				],
