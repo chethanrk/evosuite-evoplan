935f0ef1af5d9ba5e0b45bf1c9809d65dc1c8223
Giriprasad Yedulla <giriprasad.yedulla@evorait.com>
2023-04-12 05:07:45 +0000

    Changed the comment

--- webapp/controller/ErrorHandler.js
+++ webapp/controller/ErrorHandler.js
@@ -90,7 +90,7 @@ sap.ui.define([
 				item.type = oData[i].type;
 				item.description = oData[i].message;
 				item.subtitle = oData[i].message;
-				//item.counter = iCounter; used for counting the type of messages in message manager
+				//item.counter = iCounter; used for displaying the count for the types of messages in message manager
 
 				if (!JSON.stringify(aMessages).includes(JSON.stringify(item))) {
 					aMessages.push(item);
