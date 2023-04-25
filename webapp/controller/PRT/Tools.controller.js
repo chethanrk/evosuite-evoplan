4724541942f094c7eab9acbc3e0201bd77c7b37a
Sagar Poojari <sagar.poojari@evorait.com>
2023-04-25 10:30:50 +0000

    Code correction

--- webapp/controller/PRT/Tools.controller.js
+++ webapp/controller/PRT/Tools.controller.js
@@ -34,12 +34,12 @@ sap.ui.define([
 			this._oToolsTable = this.byId("idToolsTable").getTable();
 			this._configureToolDataTable(this._oToolsTable);
 			this._eventBus = sap.ui.getCore().getEventBus();
-
-			this._eventBus.subscribe("BaseController", "refreshToolsTable", this._refreshToolsTable, this);
-
+			
 			this._mParameters = {
 				bFromDemandTools: true
 			};
+
+			this._eventBus.subscribe("BaseController", "refreshToolsTable", this._refreshToolsTable, this);
 
 			this._oRouter.getRoute("demandTools").attachPatternMatched(function () {
 				this._oViewModel.setProperty("/PRT/bIsGantt", false);
