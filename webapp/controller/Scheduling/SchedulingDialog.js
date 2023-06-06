c6624cd404c45c7db7c6c1d2fd0327a8d69ec7d4
Manik Saluja <manik093@gmail.com>
2023-06-06 10:15:58 +0000

    added cahnges for the files

--- webapp/controller/Scheduling/SchedulingDialog.js
+++ webapp/controller/Scheduling/SchedulingDialog.js
@@ -9,37 +9,11 @@ sap.ui.define([
 ], function (BaseController, MessageBox, formatter, Constants, Fragment, OverrideExecution, MessageToast) {
 
 	return BaseController.extend("com.evorait.evoplan.controller.Scheduling.SchedulingDialog", {
-
+		
 		/* =========================================================== */
 		/* Public methods                                              */
 		/* =========================================================== */
 
-		/**
-		 * validations before tools assignment service call
-		 * @param aSources Selected tools data and path
-		 * @param sTargetPath Target Resource/Demand 
-		 * @param mParameters flag of source view 
-		 */
-		samplPublicMehtods: function (aSources, oTargetObj, mParameters, sTargetPath) {
-            console.log('method called sample public method');
-		},
-
-
-
-		/* =========================================================== */
-		/* Private methods                                              */
-		/* =========================================================== */
-
-		/**
-		 * method to call assignment service
-		 * @param aSources Selected tools data and path
-		 * @param oDateParams required common parameters for all the assignments 
-		 * @param mParameters flag of source view 
-		 */
-		_samplePrivateFunction: function (aSources, oDateParams, mParameters) {
-            
-
-		}
 
 
 	});
