ea1aa03d20a3ee675c3608a739e4c751206406ba
Sagar Poojari <sagar.poojari@evorait.com>
2023-07-04 13:38:10 +0000

    Added function decription for onInit()

--- webapp/controller/common/DemandTableOperations.js
+++ webapp/controller/common/DemandTableOperations.js
@@ -78,6 +78,10 @@ sap.ui.define([
 			}
 		},
 
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 **/
 		onInit: function(oEvent){
 			this.oSchedulingActions = new SchedulingActions(this);
 		},
