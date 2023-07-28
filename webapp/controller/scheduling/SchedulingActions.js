1ac60d1c78ac791a0c17e7e5e790c97626d7260c
Bhumika Ranawat <bhumika.ranawat@evorait.com>
2023-07-28 12:33:58 +0000

    EVOSUITE2-5050: ALLOW_RESCHEUDLE integrated

--- webapp/controller/scheduling/SchedulingActions.js
+++ webapp/controller/scheduling/SchedulingActions.js
@@ -66,7 +66,7 @@ sap.ui.define([
 			if (oScheduling.selectedDemandPath && oScheduling.selectedResources && (oScheduling.selectedResources.length > 0) && oScheduling.aSelectedDemandPath.length === 1) {
 				oSelectedDemandItem = this.oDataModel.getProperty(oScheduling.selectedDemandPath);
 
-				if (oSelectedDemandItem.ALLOW_REASSIGN && !(oSelectedDemandItem.NUMBER_OF_CAPACITIES > 1)) {
+				if (oSelectedDemandItem.ALLOW_RESCHEDULE) {
 					this.oViewModel.setProperty("/Scheduling/bEnableReschedule",true);
 					
 				}else {
