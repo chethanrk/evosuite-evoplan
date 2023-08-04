8ec712c0f7a697263abfdde5cb9a6608b885dbbd
Pranali Pednekar <pranali.pednekar@evorait.com>
2023-08-04 12:34:36 +0000

    EVOSUITE2-4658 - EvoPlan - Demands list - Select All button is not working - Modified Demands, GanttDemands and Map controller to handle select all functionality based on global parameter

--- webapp/controller/demands/Demands.controller.js
+++ webapp/controller/demands/Demands.controller.js
@@ -196,7 +196,7 @@ sap.ui.define([
 			oDataTable.attachRowSelectionChange(function (oEvent) {
 				var selected = this._oDataTable.getSelectedIndices(),
 					bEnable = this.getModel("viewModel").getProperty("/validateIW32Auth"),
-					sDemandPath, bComponentExist, sMsg,
+					sDemandPath, bComponentExist, sMsg, iLastIndex,
 					oViewModel=this.getModel("viewModel");
 				var iMaxRowSelection = this.getModel("user").getProperty("/DEFAULT_DEMAND_SELECT_ALL");
 
@@ -222,12 +222,22 @@ sap.ui.define([
 					this.byId("idUnassignButton").setEnabled(false);
 				}
 
+				// condition to deselect All when max selection limit is already reach but pressing select All checkbox
+				if (oEvent.getParameter("selectAll") && this._nSelectedDemandsCount === iMaxRowSelection) {
+					this._oDataTable.clearSelection();
+					return;
+				}
 				//If the selected demands exceeds more than the maintained selected configuration value
-				if (oEvent.getParameter("selectAll")) {
-					sMsg = this.getResourceBundle().getText("ymsg.allSelect", [this._aSelectedRowsIdx.length]);
-					this.showMessageToast(sMsg);
-				} else if (iMaxRowSelection <= this._aSelectedRowsIdx.length) {
-					sMsg = this.getResourceBundle().getText("ymsg.maxRowSelection", [iMaxRowSelection]);
+				if (selected.length > iMaxRowSelection) {
+					if (oEvent.getParameter("selectAll")) {
+						iLastIndex = selected.pop();
+						this._oDataTable.removeSelectionInterval(iMaxRowSelection, iLastIndex);
+						sMsg = this.getResourceBundle().getText("ymsg.allSelect", [iMaxRowSelection]);
+					} else {
+						iLastIndex = oEvent.getParameter('rowIndex');
+						this._oDataTable.removeSelectionInterval(iLastIndex, iLastIndex);
+						sMsg = this.getResourceBundle().getText("ymsg.maxRowSelection", [iMaxRowSelection]);
+					}
 					this.showMessageToast(sMsg);
 				}
 
@@ -257,6 +267,7 @@ sap.ui.define([
 				this.oSchedulingActions.validateReScheduleButton();
 
 				this.showWarningMsgResourceTree(true);
+				this._nSelectedDemandsCount = this._oDataTable.getSelectedIndices().length;
 			}, this);
 		},
 
