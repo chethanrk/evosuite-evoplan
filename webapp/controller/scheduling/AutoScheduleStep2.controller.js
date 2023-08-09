7d38f7981cf24f8c99caca1391f9e8fe50823bae
Bhumika Ranawat <bhumika.ranawat@evorait.com>
2023-08-09 07:57:53 +0000

    EVOSUITE2-5102: Filter dialog added and applied

--- webapp/controller/scheduling/AutoScheduleStep2.controller.js
+++ webapp/controller/scheduling/AutoScheduleStep2.controller.js
@@ -1,12 +1,17 @@
 sap.ui.define([
   "com/evorait/evoplan/controller/BaseController",
-  'sap/ui/model/Sorter',
-  'sap/ui/model/Filter'
-], function (BaseController, Sorter, Filter) {
+  'sap/ui/model/Filter',
+  "sap/ui/core/Fragment",
+  "sap/ui/model/FilterOperator",
+], function (BaseController, Filter, Fragment, FilterOperator) {
   "use strict";
 
   return BaseController.extend("com.evorait.evoplan.controller.scheduling.AutoScheduleStep2", {
     _oResponseTable: null,
+    _mFilters: {
+      planned: new Filter("PLANNED", FilterOperator.EQ, true),
+      nonPlanned: new Filter("PLANNED", FilterOperator.EQ, false)
+    },
 
     /**
      * on init event
@@ -17,15 +22,16 @@ sap.ui.define([
     },
 
     /**
-    * Call set filter on initial load of table
-    */
+     * Call set filter on initial load of table
+     */
     onAfterRendering: function () {
-      this._setDataFilter();
+      this._oResponseTable.getBinding("items").filter(this._setDataFilter());
+      this._oViewModel.setProperty("/Scheduling/sResponseFilterCounts", this.getResourceBundle().getText("xbut.filters") + " (0)");
     },
 
     /**
-    * Return group header key for grouping in scheduled data on resource name
-    */
+     * Return group header key for grouping in scheduled data on resource name
+     */
     fnGetResName: function (oContext) {
       if (!this._oViewModel.getProperty("/Scheduling/iSelectedResponse")) {
         return oContext.getProperty('ResourceName');
@@ -33,30 +39,79 @@ sap.ui.define([
     },
 
     /**
-    * Call set filter when radio button selected
-    */
+     * Call set filter when radio button selected
+     */
     onChangeResponseType: function () {
-      this._setDataFilter();
+      this._setCustomTableFilter();
     },
 
+    /**
+     * Opens filter dialog for response table
+     */
+    onShowFilters: function () {
+      if (!this._oResponseFilterDialog) {
+        this._oResponseFilterDialog = Fragment.load({
+          name: "com.evorait.evoplan.view.scheduling.fragments.DemandFilterDialog",
+          controller: this,
+          type: "XML"
+        }).then(function (oDialog) {
+          oDialog.addStyleClass(this._oViewModel.getProperty("/densityClass"));
+          this.getView().addDependent(oDialog);
+          return oDialog;
+        }.bind(this));
+      }
+      this._oResponseFilterDialog.then(function (oDialog) {
+        oDialog.open();
+      });
+    },
+
+    /**
+     * close filter dialog and add all seleted filters 
+     * to json response table
+     */
+    onPressAddFilterDialog: function () {
+      if (this._oResponseFilterDialog) {
+        this._oResponseFilterDialog.then(function (oDialog) {
+          this._setCustomTableFilter();
+          oDialog.close();
+        }.bind(this));
+      }
+    },
 
     /* =========================================================== */
     /* Private methods                                             */
     /* =========================================================== */
 
     /**
-    * Sets filter on data based on scheduled and non-scheduled radio btn select
-    * The filter set is on "PLANNED" boolean field from local JSON model
-    */
-    _setDataFilter: function(){
-      var aFilters=[];
+     * Sets filter on data based on scheduled and non-scheduled radio btn select
+     * The filter set is on "PLANNED" boolean field from local JSON model
+     */
+    _setDataFilter: function () {
       if (!this._oViewModel.getProperty("/Scheduling/iSelectedResponse")) { //When scheduled demands
-        aFilters.push(new Filter("PLANNED", "EQ", true));
+        return this._mFilters.planned;
       } else { //when non-scheduled demands
-        aFilters.push(new Filter("PLANNED", "EQ", false));
+        return this._mFilters.nonPlanned;
       }
-      this._oResponseTable.getBinding("items").filter(aFilters);
-    }
+    },
+
+    /**
+     * collect all filters and bind to json model table of response
+     * - Filter dialog
+     */
+    _setCustomTableFilter: function () {
+      var oSmartFilter = sap.ui.getCore().byId("listReportFilter"),
+        aFilter = [];
+
+      if (oSmartFilter) {
+        aFilter = oSmartFilter.getFilters();
+        var sFilterCount = Object.keys(oSmartFilter.getFilterData()).length;
+        this._oViewModel.setProperty("/Scheduling/sResponseFilterCounts", this.getResourceBundle().getText("xbut.filters") + " (" + sFilterCount + ")");
+      }
+
+      aFilter.push(this._setDataFilter());
+
+      this._oResponseTable.getBinding("items").filter(aFilter);
+    },
 
   });
 })
