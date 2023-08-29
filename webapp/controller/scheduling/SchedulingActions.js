84255fe88ef2d3b177ec00ad98fdf3ecb98c6aec
Manik Saluja <manik093@gmail.com>
2023-08-29 13:04:49 +0000

    EVOSUITE2-5140 fix added for date change

--- webapp/controller/scheduling/SchedulingActions.js
+++ webapp/controller/scheduling/SchedulingActions.js
@@ -546,7 +546,7 @@ sap.ui.define([
 				this.showMessageToast(this.oResourceBundle.getText("ymsg.ValidateDateStart"));
 				return bValidate;
 			}
-			if(endDate.toDate() < oMinDate || startDate.toDate() > oMaxDate){
+			if(endDate.toDate() < oMinDate || endDate.toDate() > oMaxDate){
 				bValidate=false;
 				this.showMessageToast(this.oResourceBundle.getText("ymsg.ValidateDateEnd"));
 				return bValidate
@@ -663,6 +663,8 @@ sap.ui.define([
 		 * @return {Object} - Array of allowed and not allowed demands in separate properties
 		 */
 		_checkAllowedDemands: function (oTable, aSelectedRowsIdx) {
+			var t0 = performance.now();
+
 			var aPathsData = [],
 				aNonAssignableDemands = [],
 				oData, oContext, sPath;
@@ -686,6 +688,8 @@ sap.ui.define([
 					aNonAssignableDemands.push(this.getMessageDescWithOrderID(oData, null, true));
 				}
 			}
+			var t1 = performance.now();
+			console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
 			return {
 				aPathsData: aPathsData,
 				aNonAssignable: aNonAssignableDemands,
