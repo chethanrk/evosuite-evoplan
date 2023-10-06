53cc925adb5fee2cc8903169662d1c0e4744bed6
Sagar Poojari <sagar.poojari@evorait.com>
2023-10-06 08:27:30 +0000

    Code correction

--- webapp/controller/scheduling/mapProvider/PTV.js
+++ webapp/controller/scheduling/mapProvider/PTV.js
@@ -496,7 +496,7 @@ sap.ui.define([
 					"serviceTime": aDemandsData[oDemandGuid].serviceTime
 				};
 				if (bQualificationCheck) {
-					oOrder["requiredVehicleEquipment"] = aDemandsData[oDemandGuid].qualification;
+					oOrder["requiredVehicleEquipment"] = aDemandsData[oDemandGuid].qualification.join(",");
 				}
 				orders.push(oOrder);
 			}
